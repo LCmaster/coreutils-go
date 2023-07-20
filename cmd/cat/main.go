624db7936d3b5ac216a639711868c6e7f1e0adb6
Luiz Vicente <luizcv89@gmail.com>
2023-07-20 15:11:28 -0300

    Add outline for flags to be implemented

--- cmd/cat/main.go
+++ cmd/cat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
@@ -11,13 +12,39 @@ var catCmd = &cobra.Command{
 	Short: "cat - concatenate files and print on the standard output",
 	Run: func(cmd *cobra.Command, args []string) {
 		for _, filename := range args {
-			data, err := os.ReadFile(filename)
+			file, err := os.Open(filename)
 			if err != nil {
 				panic(err)
 			}
-			fmt.Print(string(data))
+
+			fileScanner := bufio.NewScanner(file)
+			fileScanner.Split(bufio.ScanLines)
+
+			lineCount := 0
+			for fileScanner.Scan() {
+				lineCount++
+				//lines = append(lines, fileScanner.Text())
+				fmt.Printf("%d\t%s\r\n", lineCount, fileScanner.Text())
+
+			}
+
+			file.Close()
 		}
 	},
+}
+
+func init() {
+	// TODO: catCmd.Flags().BoolP("show-all", "A", false, "equivalent to -vET")
+	// TODO: catCmd.Flags().BoolP("number-nonblank", "b", false, "number nonempty output lines, overrides -n")
+	// TODO: catCmd.Flags().BoolS("esoteric-e", "e", false, "equivalent to -vE")
+	// TODO: catCmd.Flags().BoolP("show-ends", "E", false, "display $ at end of each line")
+	// TODO: catCmd.Flags().BoolP("number", "n", false, "number all output lines")
+	// TODO: catCmd.Flags().BoolP("squeeze-blank", "s", false, "suppress repeated empty output lines")
+	// TODO: catCmd.Flags().BoolS("esoteric-t", "t", false, "equivalent to -vT")
+	// TODO: catCmd.Flags().BoolP("show-tabs", "T", false, "display TAB characters as ^I")
+	// TODO: catCmd.Flags().BoolP("show-nonprinting", "v", false, "use ^ and M- notation, except for LFD and TAB")
+	catCmd.Flags().Bool("version", false, "output version information and exit")
+	catCmd.Flags().BoolP("help", "h", false, "display this help and exit")
 }
 
 func main() {
